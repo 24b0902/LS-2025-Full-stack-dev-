251415d5dac6848fa81e44b894fee289e405ffa1
24b0902 <srinidhi.dh@gmail.com>
2025-07-02 15:56:03 +0530

    Update forms.py

--- Week 3 Assignment/accounts/forms.py
+++ Week 3 Assignment/accounts/forms.py
@@ -1,4 +1,3 @@
-
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -8,10 +7,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ("username", "email", "password1", "password2")
 
     def clean_email(self):
-        email = self.cleaned_data['email']
+        email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email already exists.")
+            raise forms.ValidationError("Email already exists")
         return email
