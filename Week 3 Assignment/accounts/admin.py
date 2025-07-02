c179e8ac058d20b4f53e058f1de92610ed041371
24b0902 <srinidhi.dh@gmail.com>
2025-07-02 15:40:51 +0530

    Update admin.py

--- Week 3 Assignment/accounts/admin.py
+++ Week 3 Assignment/accounts/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 
-# Register your models here.
-from django.contrib.auth.models import User
+User = get_user_model()  # Always works, even for custom User models
 
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(DefaultUserAdmin):
     list_display = ['username', 'email', 'is_active', 'is_staff']
     search_fields = ['username', 'email']
     ordering = ['username']
 
+# Unregister and re-register safely
 try:
     admin.site.unregister(User)
 except admin.sites.NotRegistered:
-    pass  # User model was not registered yet, ignore the error
-admin.site.register(User, UserAdmin)
+    pass
 
+admin.site.register(User, CustomUserAdmin)
+
+# Branding
 admin.site.site_header = "YouTube Clone Admin"
 admin.site.site_title = "YT Admin"
 admin.site.index_title = "Dashboard"
