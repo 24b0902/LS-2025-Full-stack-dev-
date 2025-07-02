33f9806fad67775e37f8564740cbf4d0eaa722ff
24b0902 <srinidhi.dh@gmail.com>
2025-07-02 15:57:42 +0530

    Update urls.py

--- Week 3 Assignment/accounts/urls.py
+++ Week 3 Assignment/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import register_view, verify_view, login_view, logout_view, dashboard_view
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('verify/<str:token>/', verify_view, name='verify'),
-    path('', lambda request: redirect('login/')),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('dashboard/', dashboard_view, name='dashboard'),
-    path('verify/<str:token>/', verify_view, name='verify'),
+    path("", lambda request: redirect("login/")),  # Root of accounts redirects to login
+    path("register/", register_view, name="register"),
+    path("verify/<str:token>/", verify_view, name="verify"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("dashboard/", dashboard_view, name="dashboard"),
 ]
